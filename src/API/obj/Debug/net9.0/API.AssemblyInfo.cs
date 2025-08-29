8e8cdb2d4fbc981fe3b085b7668540b0f0969a41
RPW-11 <kriegcom77@gmail.com>
2025-08-29 15:45:42 +0800

    change rgv plan data path

--- src/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ src/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ef65d993d243ffe9d706d203ef8d010845bbec4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b44ad722329473cee02abd915d3c3d06ffd52bfc")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
